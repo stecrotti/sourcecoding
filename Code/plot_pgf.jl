693baf7320b8b33e1d00a8eaff9e4cea0841b25d
stecrotti <ruggentipini@gmail.com>
2020-07-03 17:12:13 +0200

    plotting for latex

---
+++ Code/plot_pgf.jl
@@ -0,0 +1,19 @@
+# using PyPlot
+
+plt.rcdefaults()
+rcParams = PyPlot.PyDict(PyPlot.matplotlib."rcParams")
+rcParams["pgf.rcfonts"] = false
+rcParams["backend"] = "pgf"
+rcParams["text.usetex"] = true
+rcParams["pgf.texsystem"] = "xelatex"
+rcParams["font.family"] = "serif"
+rcParams["font.serif"] = ""
+
+# PyPlot.close("all")
+# plt.ioff()
+# PyPlot.plot([1,2,3], [4,5,6], label="bu")
+# PyPlot.legend()
+# plt.:xlabel("X label")
+# plt.:ylabel("Y label")
+
+# plt.savefig("../../../../provetex/test.pgf", format="pgf")

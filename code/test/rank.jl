d962a23a5c3fe483140f66f64769119b071779e1
stecrotti <ruggentipini@gmail.com>
2020-11-19 14:38:57 +0100

    script for checks on ranks

--- code/test/rank.jl
+++ code/test/rank.jl
@@ -1,8 +1,8 @@
 include("./../headers.jl")
 
 q = 2
-n = 200
-mvals = [50 100 150 180]
+n = 500
+mvals = [100 200 300 400]
 navg = 100
 seed = 12345
 
@@ -11,12 +11,19 @@ exceptions = LossyModel[]
 for m in mvals
     for i = 1:navg  
         lm = LossyModel(q,n,m,randseed=seed+i)
-        if rank(lm) != m-1
+        rk = rank(lm)
+        if rk != m-1
             g = only_factors_graph(lm.fg)
-            if length(connected_components(g))!=m-rank(lm)
+            if length(connected_components(g))!=m-rk
+                push!(exceptions,lm)
+            end
+            breduction!(lm)
+            if rank(lm) != rk
                 push!(exceptions,lm)
             end
         end
     end
     println("m = $m finished")
 end
+
+exceptions

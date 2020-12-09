af45a1ba882066b68a55dd12544b02384846fcb0
Louise Budzynski <louise.budzynski@gmail.com>
2020-12-09 18:54:33 +0100

    Update

--- ExpectedWEF/SolverWEF_randomsource.jl
+++ ExpectedWEF/SolverWEF_randomsource.jl
@@ -4,7 +4,7 @@ function solalpha(delta, w, p)
 	if delta == 1
 		sol = 0
 	else
-	    A = delta^2
+	    A = delta
     	B = delta*( (1-p)/p + p/(1-p) ) - (1-w)*p/(1-p) - w*(1-p)/p
     	C = delta - 1
     	Delta = B^2-4*A*C
@@ -20,7 +20,7 @@ end
 function Xsi(delta, w, p)
 	if delta == 0 #then u0=0 and u1=0
 		res = w*log(p) + (1 - w)*log(1-p)
-	elseif delta == 1
+	elseif delta == 1 #then u0=1-w and u1=w
 		res = (1-w)*log(p) + w*log(1-p)
 	else
 	    alpha = solalpha(delta, w, p)
@@ -31,29 +31,35 @@ function Xsi(delta, w, p)
     return res
 end
 
-function Psi(delta, p, lambda1, lambda2, k, y0)
+function Psi(delta, p, lambda1, P, y0, len::Int64=100)
+# increase 'len' the length of the vector Xs+Ph to increase precision in finding maximum
 	if p == 0
-    	res = Phi(delta, lambda1, lambda2, k, y0)
+    	res = Phi(delta, lambda1, P, y0)
     elseif p == 1
-    	res = Phi(1 - delta, lambda1, lambda2, k, y0)
+    	res = Phi(1 - delta, lambda1, P, y0)
     else
-		W=range(0.01,0.99,length=500)
-		P=Phi.(W, lambda1, lambda2, k, y0)
+		W=range(0.001,0.999,length=len)
+		tP(w)=Phi(w, lambda1, P, y0)
+		Ph=tP.(W)
 		Xs=Xsi.(delta, W, p)
-		res = findmax(Xs+P)
-		res = res[1] # look also at W[res[2]]
+		res = findmax(Xs+Ph)
+		#println("position of max: ", W[res[2]]) # look at the position of the maximum
+		res = res[1] 
 	end
     return res
 end
 
-function zeroPsi(p, lambda1, k, y0)
-	lambda2 = 1 - lambda1
-	if p == 0
-		delta0 = 0
-	elseif p == 1
-		delta0 = 0
+function zeroPsi(p, lambda1, P, y0, len::Int64=100, tol::Float64=10^(-10))
+# set tolerance tol=xatol for bisection algorithms
+	dinit = 0.001
+	dend = 0.5
+	Pinit = Psi(dinit, p, lambda1, P, y0, len)
+	Pend = Psi(dend, p, lambda1, P, y0, len)
+	if Pinit*Pend > 0
+		#println("same sign")
+		delta0 = 0.
 	else
-		delta0 = find_zero(delta -> Psi(delta, p, lambda1, lambda2, k, y0), (0.001,0.5))
+		delta0 = find_zero(delta -> Psi(delta, p, lambda1, P, y0, len), (dinit,dend); xatol = tol)
 	end
 	return delta0
 end

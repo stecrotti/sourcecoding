5666e30dc31404abd3a591677b528192d7cd39b9
Alfredo Braunstein <alfredo.braunstein@polito.it>
2021-02-27 15:39:29 +0100

    make cavity work on source generators and arbitrary indexed dest

--- code/cavity.jl
+++ code/cavity.jl
@@ -1,28 +1,30 @@
 # computes in linear time and inplace dst[i] = 
 # src[1] op … src[i-1] op src[i+1] op … src[end] op init
 # returns src[1] op … op src[end] op init
-# 1-base indexing required
 
 function cavity!(dest, source, op, init)
     @assert length(dest) == length(source)
     isempty(source) && return init
     if length(source) == 1
         dest[begin] = init 
-        return op(source[begin], init)
+        return op(first(source), init)
     end
     if length(source) == 2
-        dest[begin] = op(source[end], init)
-        dest[end] = op(source[begin], init)
-        return op(dest[begin], source[begin])
+        dest[begin] = op(first(Iterators.reverse(source)), init)
+        dest[end] = op(first(source), init)
+        return op(dest[begin], first(source))
     end
-    accumulate!(op, dest, source)
+    dest[begin], rest = Iterators.peel(source)
+    for (s,i) = zip(rest, eachindex(dest))
+        dest[i+1] = op(dest[i], s)
+    end
     full = op(dest[end], init)
     right = init
-    for i=length(source):-1:2
+    for (i,s)=zip(lastindex(dest):-1:firstindex(dest)+1,Iterators.reverse(source))
         dest[i] = op(dest[i-1], right);
-        right = op(source[i], right);
+        right = op(s, right);
     end
-    dest[1] = right
+    dest[begin] = right
     full
 end
 

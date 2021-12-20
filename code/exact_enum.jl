291e0e349aea583737a9fbb3ed4be8d4eee95153
stecrotti <ruggentipini@gmail.com>
2021-12-20 13:49:20 +0100

    lightest basis and other exploration code

--- code/exact_enum.jl
+++ code/exact_enum.jl
@@ -13,7 +13,7 @@ end
 
 distortion(x::Integer, y::Integer) = count_ones( xor(x, y) )
 
-function hamming_weight(x::BitVector)
+function hamming(x::BitVector)
     w = 0
     for xc in x.chunks
         w += count_ones(xc)
@@ -93,10 +93,10 @@ function exact_wef(B, sources=BitVector[]; showprogress=true,
     for i in 0:2^k-1
         x.chunks[1] = i 
         bitmult_fast!(y, BB, x)
-        d = hamming_weight(y)
+        d = hamming(y)
         h0[d+1] += 1
         for (j,s) in enumerate(sources)
-            d = distortion(y, s)
+            d = hamming(y, s)
             h[j][d+1] += 1
             if d < mins[j]
                 mins[j] = d
@@ -183,6 +183,183 @@ function plot_wef_prob(h::Vector{Int}, β::Real, kw...)
     Plots.plot!(pl; kw...)
     pl
 end
+
+### LIGHTEST BASIS    
+
+function cws_of_weight_w(BB, w::Int, n::Int; 
+        y = falses(n), x = falses(size(BB,2)))
+    nn, k = size(BB)
+    @assert mod(nn, 64)==0 "nn=$nn"
+    cws = typeof(x.chunks[1])[]
+    for i in 0:2^k-1
+        x.chunks[1] = i 
+        bitmult_fast!(y, BB, x)
+        d = hamming(y)
+        if d == w
+            push!(cws, i)
+        end
+    end
+    cws
+end
+
+function islinearindep(B, v; Baux=[B v])
+    gfrcefGF2!(Baux)
+    return !all(iszero, Baux[:,end])
+end
+
+function lightest_basis(BB, n::Int; y = falses(n), x = falses(size(BB,2)))
+    nn, k = size(BB)
+    @assert mod(nn, 64)==0 "nn=$nn"
+    Blight = falses(nn, 0)
+    prog = ProgressUnknown(desc="Finding lightest basis", dt=10)
+    for w in 1:n
+        cws = cws_of_weight_w(BB, w, n; y=y, x=x)
+        for c in cws 
+            z = bitmult_fast(BB, c)
+            # try to add c to the basis
+            islinearindep(Blight, z) && (Blight = [Blight z])
+            # if basis complete, return
+            size(Blight, 2) == k && return Blight, w
+        end
+        next!(prog, showvalues=[("weight", w)])
+    end
+    error("Something went wrong")
+end
+
+### STUFF FOR CLUSTER EXPLORATION 2 NOTEBOOK
+
+# save idx and distortion of the 2 best codewords
+function exact_wef2(B, sources=BitVector[]; showprogress=true,
+    y = BitVector(undef, size(B,1)),
+    x = BitVector(undef, size(B,2)),
+    h0 = zeros(Int, size(B,1)+1),
+    h = [zeros(Int, size(B,1)+1) for _ in sources])
+
+    n, k = size(B)
+    @assert k < 64 "Adjust code for k larger than 64" 
+
+    r = 64 * ( floor(Int, n/64) + 1)
+    # extend B so that #rows is a multiple of 64 => easier for multiplication
+    BB = [B; falses(r-n, k)]
+    
+    m1 = fill(Inf, length(sources)); m2 = copy(m1)
+    idx1 = zeros(Int, length(sources)); idx2 = copy(idx1)
+    
+    dt = showprogress ? 1.0 : Inf
+    prog = ProgressMeter.Progress(2^k, dt=dt, desc="Exact WEF for 2 closest CWs ")
+    for i in 0:2^k-1
+        x.chunks[1] = i 
+        bitmult_fast!(y, BB, x)
+        d = hamming(y)
+        h0[d+1] += 1
+        for (j,s) in enumerate(sources)
+            d = hamming(y, s)
+            h[j][d+1] += 1
+            if d < m1[j]
+                m2[j] = m1[j]; m1[j] = d
+                idx2[j] = idx1[j]; idx1[j] = i
+            elseif d < m2[j]
+                idx2[j] = i; m2[j] = d
+            end
+        end
+        ProgressMeter.next!(prog)
+    end
+    (m1, m2) ./ n, (idx1, idx2)
+end
+
+# compute the threshold value of distortion so that a proportion α of the 
+#  Boltzmann probability mass is made up by codewords with less than that dist
+function distortion_thresh(h, β, α=0.99)
+    @assert α ≤ 1
+    p = [h[i]*exp(-β*i) for i in eachindex(h)]
+    p ./= sum(p)
+    c = 0.0
+    for i in eachindex(p)
+        c += p[i]
+        c ≥ α && return (i-1) / length(h), sum(@view h[1:i])
+    end
+    return (lastindex(p)-1) / length(h), sum(@view h[1:i])  
+end
+
+# count codewords up to distortion maxdistortion
+function ncw(h, maxdistortion)   
+    nc = 0
+    n = length(h) - 1
+    for i in eachindex(h)
+        nc += h[i]
+        ((i-1) / n) ≥ maxdistortion && return nc
+    end
+    error("something went wrong")
+end
+
+# compute A = {indices of cws up to weight maxdistance}
+#  xor of A with any codeword gives its "neighbors"
+function neighbors(BB, maxdistance::Int, ref::BitVector,
+        y = falses(length(ref)), x = falses(size(BB,2)))
+    nn, k = size(BB)
+    @assert mod(nn, 64)==0 "nn=$nn"
+    neigs = typeof(x.chunks[1])[]
+    dist = Int[]
+    for i in 0:2^k-1
+        x.chunks[1] = i 
+        bitmult_fast!(y, BB, x)
+        y == ref && continue
+        d = hamming(y, ref)
+        if d <= maxdistance
+            push!(neigs, i)
+            push!(dist, d)
+        end
+    end
+    neigs, dist
+end
+
+# connected component of codeword `ref`
+function connected_component_fast(B, maxdistortion::Real, maxdistance::Int, ref::Integer, 
+        s::BitVector; ncodewords = 0, showprogress=true,
+        y = BitVector(undef, size(B,1)), x = BitVector(undef, size(B,2)),
+        z = BitVector(undef, size(B,1)), t = BitVector(undef, size(B,2)),
+        target::Integer=-1)
+    BB = augment_basis(B)
+    n, k = size(B)
+    visited = OffsetArray(falses(2^k), -1)
+    Q = [ref]
+    neigs, _ = neighbors(BB, maxdistance, falses(size(BB,1)))
+    neigs_aux = deepcopy(neigs)
+    dt = showprogress ? 1.0 : Inf
+    prog = ProgressMeter.ProgressUnknown(dt=dt, desc="Codewords visited")
+    while !isempty(Q)
+        w = popfirst!(Q)
+        w == target && (visited[w] = true; return visited)
+        visited[w] == true && continue
+        visited[w] = true
+        neigs_aux .= xor.(w, neigs)
+        for r in neigs_aux
+            x.chunks[1] = r
+            bitmult_fast!(y, BB, x)
+            distortion(y, s) <= maxdistortion && push!(Q, r)
+        end   
+        st = ncodewords == 0 ? "" : "/"*string(ncodewords)
+        ProgressMeter.next!(prog, showvalues=[("added to cc", "$(sum(visited))"*st), (:ncodewords,2^k)])
+    end
+    visited    
+end
+
+# returns the first distortion for which c1, c2 are in the same connected component
+function two_cw_in_same_cc(B, maxdistortions, maxdistance::Int, 
+        c1::Integer, c2::Integer,
+        s::BitVector; ncodewords=zeros(Int, length(maxdistortions)),
+        showprogress=true)
+    for (i, maxdistortion) in enumerate(maxdistortions)
+        visited1 = connected_component_fast(B, maxdistortion, maxdistance, c1, s, ncodewords=ncodewords[i],
+        showprogress=showprogress, target=c2)
+        # visited2 = connected_component_fast(B, maxdistortion, maxdistance, c2, s, ncodewords=ncodewords[i],
+        # showprogress=showprogress, target=c1)
+        visited1[c2] && return maxdistortion
+        # (visited1[c2] || visited2[c1]) && return maxdistortion
+    end
+    return Inf
+end
+
 
 # multiplies B*x given the transpose of B 
 # function bitmult!(y::BitVector, Bt::SparseMatrixCSC{Bool,Int}, x::BitVector)
